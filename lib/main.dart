4e59f9803ace6f44918761b1864043d40b3ec486
duclt <duclt@ghtk.vn>
2022-12-08 23:08:16 +0700

    update widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return  MaterialApp(
       title: 'Startup Name Generator',
       theme: ThemeData(
@@ -19,96 +18,40 @@ class MyApp extends StatelessWidget {
           foregroundColor: Colors.black
         ),
       ),
-      home: RandomWords(),
+      home: SampleAppPage(),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
+class SampleAppPage extends StatefulWidget {
+  const SampleAppPage({super.key});
 
   @override
-  State<RandomWords> createState() => _RandomWordsState();
+  State<SampleAppPage> createState() => _SamplePageState();
 }
 
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _saved = <WordPair>{};
-  final _biggerFont = const TextStyle(fontSize: 18);
+class _SamplePageState extends State<SampleAppPage> {
 
-  @override
-  Widget build(BuildContext context) {
+  String textToShow = 'I Like Flutter';
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-        actions: [
-          IconButton(onPressed: _pushSaved, icon: const Icon(Icons.list), tooltip: 'Saved Sugguestions',)
-        ],
-      ),
-      body: ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemBuilder: (context, i) {
-            if (i.isOdd) return const Divider();
-            final index = i ~/2;
-            if (index >= _suggestions.length) {
-              _suggestions.addAll(generateWordPairs().take(10));
-            }
-
-            final alreadySaved = _saved.contains(_suggestions[index]);
-
-            return ListTile(
-              title: Text(
-                _suggestions[index].asPascalCase,
-                style: _biggerFont,
-              ),
-              trailing: Icon(
-                alreadySaved ? Icons.favorite : Icons.favorite_border,
-                color: alreadySaved ? Colors.red : null,
-                semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-              ),
-              onTap: () {
-                setState(() {
-                  if (alreadySaved) {
-                    _saved.remove(_suggestions[index]);
-                  } else {
-                    _saved.add(_suggestions[index]);
-                  }
-                });
-              },
-            );
-          })
-    );
+  void _updateText() {
+    setState(() {
+      textToShow = 'Flutter is Awsowme!';
+    });
   }
-
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final titles = _saved.map(
-                (pair) {
-                    return ListTile(
-                            title: Text(
-                              pair.asPascalCase,
-                              style: _biggerFont,
-                          ),
-                    );
-                 },
-          );
-
-          final divided = titles.isNotEmpty
-                ? ListTile.divideTiles(tiles: titles,context: context).toList() : <Widget>[];
-
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Sugguestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Sample App')),
+        body: Center(child: Text(textToShow)),
+          floatingActionButton: FloatingActionButton(
+          onPressed: _updateText,
+            tooltip: 'Update Text',
+            child: const Icon(Icons.update),
+        ),
+      );
+    }
 
 }
+
 
