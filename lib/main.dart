e259ff7fcdce4e62413e77d7bea6d986705f5d18
duclt <duclt@ghtk.vn>
2022-12-10 16:06:33 +0700

    network call

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -33,37 +34,42 @@ class SampleAppPage extends StatefulWidget {
 
 class _SamplePageState extends State<SampleAppPage> {
 
-  bool toggle = true;
-
-  void _toggle() {
+  List<Map<String, dynamic>> data = <Map<String, dynamic>>[];
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+  
+  Future<void> loadData() async {
+    final Uri dataURL = Uri.parse('https://jsonplaceholder.typicode.com/posts');
+    final http.Response response = await http.get(dataURL);
     setState(() {
-      toggle = !toggle;
+      data = jsonDecode(response.body);
     });
   }
-
-  Widget _getToggleChild() {
-    if (toggle) {
-      return const Text('Toggle One');
-    } else {
-      return CupertinoButton(
-        onPressed: (){},
-        child: const Text('Toggle Two'),
-      );
-    }
+  
+  Widget getRow(int index) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Text('Row ${data[index]['title']}'),
+    );
   }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(title: const Text('Sample App')),
-        body: Center(child: _getToggleChild()),
-          floatingActionButton: FloatingActionButton(
-          onPressed: _toggle,
-            tooltip: 'Update Text',
-            child: const Icon(Icons.update),
-        ),
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample App'),
+      ),
+      body: ListView.builder(itemBuilder: (context, index){
+        return getRow(index);
+      }, itemCount: data.length,),
+    );
+  }
 
 }
 
